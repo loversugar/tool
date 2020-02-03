e6706557180a480b2324ed4284131441722c3d4e
totti <253262275@qq.com>
2020-02-03 12:21:22 +0800

    添加创建shadowsocks服务器脚本

---
+++ create-ssserver.sh
@@ -0,0 +1,20 @@
+
+#获取权限
+sudo -s
+
+# 更新apt-get
+
+apt-get update
+
+# 安装python包管理工具
+
+apt-get install python-setuptools
+
+apt-get install python-pip
+
+# 安装shadowsocks
+
+pip install shadowsocks
+
+#安装libsodium:
+sudo apt install libsodium-dev
